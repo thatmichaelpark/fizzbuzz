db309e4aa27d06feef0b0f59bd0316b03fabc5a9
Michael Park <thatmichaelpark@gmail.com>
2016-03-06 01:33:35 -0800

    refactored; input w/simple validation

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,27 @@
+'use strict'
+
 $(document).ready(function () {
 
 	function print(a) {
 		$('body').append(a + '<br>');
 	}
 
-	for (var i=1; i<=100; ++i) {
-		var fizzbuzz = '';
-		if (i % 3 == 0) fizzbuzz = 'Fizz';
-		if (i % 5 == 0) fizzbuzz += 'Buzz';
-		fizzbuzz = fizzbuzz || i;
-		print(fizzbuzz);
+	function FizzBuzz(limit) {
+		for (var i=1; i<=limit; ++i) {
+			var fizzbuzz = '';
+			if (i % 3 == 0) fizzbuzz = 'Fizz';
+			if (i % 5 == 0) fizzbuzz += 'Buzz';
+			fizzbuzz = fizzbuzz || i;
+			print(fizzbuzz);
+		}
 	}
+
+	var limit = parseInt(prompt('Enter upper limit'));
+
+	if (isNaN(limit)) {
+		print('Invalid input for limit');
+	} else {
+		FizzBuzz(limit);
+	}
+
 });
